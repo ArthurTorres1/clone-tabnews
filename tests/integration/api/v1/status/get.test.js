4d41d67809bbb8322fd68c0e35fb9404824eb3d8
ArthurTorres1 <arthurtorres059@gmail.com>
2026-01-20 16:34:11 -0300

    finalizando o endpoint de `api/v1/status` com query injection

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,5 +1,14 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
-  console.log(response.status);
+
+  const responseBody = await response.json();
+  console.log(responseBody);
+
+  const parsedUpadatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpadatedAt);
+
+  expect(responseBody.dependencies.database.version).toEqual("16.11");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
